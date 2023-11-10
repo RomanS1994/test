f1ff8bf61e87411e4a63edf9b433f0d150a6616b
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-11-10 09:16:13 +0100

    sdasd

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// // Change code below this line
+// const pricePerItem = 2000+1500;
+// const productName = "Repair droid" ;-------9-----------------
+// <,,<  
+// let num = 5; 
+// console.log(num - 3)
+
+// const result = 108 + ((223 - 2)) * 5;
+// console.log(result)
+
+
+// console.log(Math.floor(value))
+// console.log(Math.ceil(value))
+// console.log(Math.round(value))
+
+//  $
+
+//  // 1. Оголошення параметрів x, y, z
+// function multiply(x, y, z) {
+//     console.log(`Результат множення дорівнює ${x + y + z}`);
+//   }
+  
+//   // 2. Передача аргументів
+//   multiply(2, 3, 5); // Результат множення дорівнює 30
+//   multiply(4, 8, 12); // Результат множення дорівнює 384
+//   multiply(17, 6, 25); // Результат множення дорівнює 2550
+
+function add(a, b, c) {
+    // Change code below this line
+  return a + b + c
+  
+  
+    // Change code above this line
+  }
+  
+  add(2, 5, 8); // 15
+  
+  console.log(add(15, 27, 10));
+  console.log(add(10, 20, 30));
+  console.log(add(5, 10, 15));f
